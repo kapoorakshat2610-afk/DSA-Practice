10ab1c626c2ef96e76c93432d99d8041169ec9de
kapoorakshat2610-afk <kapoorakshat2610@gmail.com>
2026-07-07 21:57:10 +0530

    Time: 0 ms (100%), Space: 7.9 MB (100%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.c
@@ -0,0 +1,35 @@
+
+
+bool isPowerOfTwo(int n) {
+    return n == (int)pow(2, 0) ||
+           n == (int)pow(2, 1) ||
+           n == (int)pow(2, 2) ||
+           n == (int)pow(2, 3) ||
+           n == (int)pow(2, 4) ||
+           n == (int)pow(2, 5) ||
+           n == (int)pow(2, 6) ||
+           n == (int)pow(2, 7) ||
+           n == (int)pow(2, 8) ||
+           n == (int)pow(2, 9) ||
+           n == (int)pow(2,10) ||
+           n == (int)pow(2,11) ||
+           n == (int)pow(2,12) ||
+           n == (int)pow(2,13) ||
+           n == (int)pow(2,14) ||
+           n == (int)pow(2,15) ||
+           n == (int)pow(2,16) ||
+           n == (int)pow(2,17) ||
+           n == (int)pow(2,18) ||
+           n == (int)pow(2,19) ||
+           n == (int)pow(2,20) ||
+           n == (int)pow(2,21) ||
+           n == (int)pow(2,22) ||
+           n == (int)pow(2,23) ||
+           n == (int)pow(2,24) ||
+           n == (int)pow(2,25) ||
+           n == (int)pow(2,26) ||
+           n == (int)pow(2,27) ||
+           n == (int)pow(2,28) ||
+           n == (int)pow(2,29) ||
+           n == (int)pow(2,30);
+}
