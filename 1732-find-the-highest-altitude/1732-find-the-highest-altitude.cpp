f1e74ade5b326154d5fbc99fdc6f04ed79b8d5b4
kapoorakshat2610-afk <kapoorakshat2610@gmail.com>
2026-07-07 21:17:35 +0530

    Time: 0 ms (100%), Space: 11.4 MB (9.65%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        vector<int> ans(n + 1, 0);
+        int maximum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i - 1] + gain[i - 1];
+            maximum = max(maximum, ans[i]);
+        }
+
+        return maximum;
+    }
+};
