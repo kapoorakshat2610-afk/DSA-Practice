9c96977bd3ca987346d00180fccc7faa45ac3deb
kapoorakshat2610-afk <kapoorakshat2610@gmail.com>
2026-07-24 23:53:00 +0530

    Time: 3 ms (53.95%), Space: 12.1 MB (28.37%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        int count = 0;
+
+        for(int x : nums)
+        {
+            if(st.count(x + diff) &&
+               st.count(x + 2 * diff))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
