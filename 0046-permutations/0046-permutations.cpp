8f1a3e639316322c1a13d758996b3991a913fab6
kapoorakshat2610-afk <kapoorakshat2610@gmail.com>
2026-09-22 23:54:30 +0530

    Time: 0 ms (100%), Space: 10.6 MB (70.37%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+     void getperms(vector<int>&nums,int indx,vector<vector<int>>&ans)
+     {
+        if(indx==nums.size()){
+            ans.push_back({nums});
+            return;
+        }
+        for (int i=indx;i<nums.size();i++)
+        {
+            swap(nums[indx],nums[i]);
+            getperms(nums,indx+1,ans);
+
+            swap(nums[indx],nums[i]);
+        }
+
+     }
+    vector<vector<int>> permute(vector<int>& nums) {
+         vector<vector<int>>ans;
+         getperms(nums,0,ans);
+         return ans;
+    }
+};
